03536dfd992173a3a44c80f1bc765d810657fdcd
Priyanshi Bhikadiya <priyanshi.bhikadiya@sarvadhi.com>
2023-05-14 11:26:46 +0530

    sync github with local xcode

---
+++ Discount calculator/DiscountViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DiscountViewController.swift
+//  Discount calculator
+//
+//  Created by SARVADHI on 10/05/23.
+//
+
+import UIKit
+
+class DiscountViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
